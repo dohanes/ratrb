64527a4fa5d1b251e68e2b2c612a62c756032b82
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-01-29 17:53:59 -0500

    Undeclare global variable "prefix"

--- bin/rat.rb
+++ bin/rat.rb
@@ -2,8 +2,6 @@ require 'discordrb'
 require 'dotenv/load'
 require './lib/bot'
 
-$prefix = 'r!'
-
 bot = Rat.new token: ENV['TOKEN'], prefix: 'r!', lfm_key: ENV['LASTFM_KEY'], lfm_secret: ENV['LASTFM_SECRET']
 
 bot.run
