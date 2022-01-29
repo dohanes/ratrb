398c13868df2909521184c1f433530b9ed540893
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-01-29 17:49:45 -0500

    Implement original bm command functionality

--- lib/modules/meters.rb
+++ lib/modules/meters.rb
@@ -1,16 +1,31 @@
+MARKS = ['!', '?', '.', '…']
+
 class MetersCommands
   def initialize(bot)
     bot.command(:bm, description: 'Ask rat if something is Based or Cringe') do |event, *text|
       text = text.join(' ')
-      text = event.author.username if text == ''
 
       text.gsub! '@', ''
       text.gsub! '*', ''
       text.gsub! '`', ''
       text.gsub! '_', ''
 
-      r = Random.new(text.unpack1('B*').to_i)
-      event.respond('**' + text + "** are #{r.rand >= 0.5 ? '**Based...**' : '**Cringe...**'}")
+      if text == ''
+        text = event.author.username
+        r = Random.new(text.unpack1('B*').to_i)
+
+        type = 'Cringe'
+        mark = '!?'
+        times = 30 + (r.rand * 70).floor
+      else
+        r = Random.new(text.unpack1('B*').to_i)
+
+        type = r.rand >= 0.5 ? 'Based' : 'Cringe'
+        mark = MARKS[(r.rand * MARKS.length).floor]
+        times = (r.rand * 7).floor
+      end
+
+      event.respond("**#{text}** are **#{type}#{mark * times}**")
     end
   end
 end
